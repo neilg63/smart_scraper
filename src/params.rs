d0f762d46bc7c5e5aaa484a0951beb22d2926f67
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2023-12-02 22:34:58 +0000

    added pm2-compatible run.sh

---
+++ src/params.rs
@@ -0,0 +1,52 @@
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
+
+#[skip_serializing_none]
+#[derive(Deserialize, Clone)]
+pub struct QueryParams {
+  pub uri: Option<String>,
+  pub full: Option<u8>,
+  pub elements: Option<u8>,
+  pub target: Option<String>,
+}
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PostParams {
+  pub uri: Option<String>,
+  pub full: Option<bool>,
+  pub elements: Option<bool>,
+  pub links: Option<bool>,
+  pub target: Option<String>,
+  pub targets: Option<Vec<TargetConfig>>,
+  pub raw: Option<bool>,
+  pub related: Option<bool>,
+  pub keep_media: Option<bool>,
+}
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct TargetConfig {
+  pub kind: TargetKind,
+  pub paths: Vec<String>,
+  pub mode: TargetMode,
+  pub multiple: Option<bool>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum TargetKind {
+  MainText,
+  Summaries,
+  DomainLinks,
+  ExternalLinks,
+  AllLinks,
+  Data
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum TargetMode {
+  FirstMatch,
+  MatchToMinMax,
+  MatchAll,
+  MatchOne,
+}
+
